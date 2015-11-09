4f8f938a8a634c0bda91960ad97a006629761ae7
Jamie Syme <jamie.d.syme@gmail.com>
2015-11-08 16:37:46 -0800

    Moved old ray-vs-aabb code to raycaster.js.

--- draw.js
+++ draw.js
@@ -1,6 +1,8 @@
 
 var Draw = {};
 
+
+
 // Clear the screen.
 
 Draw.clear = function() {
@@ -12,116 +14,7 @@ Draw.clear = function() {
 	}
 };
 
-// Test ray-vs-box.
-//
-// Parameters:
-//	ray = {
-//		x: 0,
-//		y: 0,
-//		dx: 0,
-//		dy: 0
-//	};
-//	aabb = {
-//		x: 0,
-//		y: 0,
-//		w: 0,
-//		h: 0 
-//	};
-//
-// Returns:
-//	null OR
-//	{
-//		x:    0, // Intersection x
-//		y:    0  // Intersection y
-//	};
-//
-Draw.testRayAABB = function(ray, aabb) {
-	
-	//
-	// Given tx1, tx2, ty1, ty2, return the intercept or null.
-	//
-	function getIntercept(tx1, tx2, ty1, ty2) {
-		// Calculate the max t
-		// This will be used to "throw out" values
-		// ie. if t > tMax, it's considered invalid
-		var tMax  = Math.max(tx1, tx2, ty1, ty2);
-		
-		// Throw out negative values (those hits are behind us)
-		if (tx1 < 0) tx1 = tMax + 1;
-		if (tx2 < 0) tx2 = tMax + 1;
-		if (ty1 < 0) ty1 = tMax + 1;
-		if (ty2 < 0) ty2 = tMax + 1;
-		
-		// Check tx1 bounds
-		if (ray.y + ray.dy * tx1 < aabb.y ||
-			  ray.y + ray.dy * tx1 > aabb.y + aabb.h)
-			tx1 = tMax + 1;
-	
-		// Check tx2 bounds
-		if (ray.y + ray.dy * tx2 < aabb.y ||
-			  ray.y + ray.dy * tx2 > aabb.y + aabb.h)
-			tx2 = tMax + 1;
-			
-		// Check ty1 bounds
-		if (ray.x + ray.dx * ty1 < aabb.x ||
-			  ray.x + ray.dx * ty1 > aabb.x + aabb.w)
-			ty1 = tMax + 1;
-	
-		// Check ty2 bounds
-		if (ray.x + ray.dx * ty2 < aabb.x ||
-			  ray.x + ray.dx * ty2 > aabb.x + aabb.w)
-			ty2 = tMax + 1;
-			
-		// Return the closest hit (if exists)
-		var tHit = Math.min(tx1, tx2, ty1, ty2);
-		if (tHit > tMax)
-			return null;
-		return {
-			x: ray.x + ray.dx * tHit,
-			y: ray.y + ray.dy * tHit
-		};
-	}
-	
-	// Handle vertical line
-	if (ray.dx === 0 && Math.abs(ray.dy) === 1) {
-	
-		// Check X bounds
-		if (ray.x < aabb.x || ray.x > aabb.x + aabb.w)
-			return null;
-			
-		// Calculate y intercepts
-		var ty1 = (aabb.y - ray.y) / ray.dy;
-		var ty2 = (aabb.y + aabb.h - ray.y) / ray.dy;
-		
-		// Find the hit
-		return getIntercept(-1, -1, ty1, ty2);
-	}
-	
-	// Handle horizontal line
-	if (Math.abs(ray.dx) === 1 && ray.dy === 0) {
-	
-		// Check Y bounds
-		if (ray.y < aabb.y || ray.y > aabb.y + aabb.h)
-			return null;
-			
-		// Calculate x intercepts
-		var tx1 = (aabb.x - ray.x) / ray.dx;
-		var tx2 = (aabb.x + aabb.w - ray.x) / ray.dx;
-		
-		// Find the hit
-		return getIntercept(tx1, tx2, -1, -1);
-	}
-	
-	// Handle every other line
-	// Calculate intercepts
-	var tx1 = (aabb.x - ray.x) / ray.dx;
-	var tx2 = (aabb.x + aabb.w - ray.x) / ray.dx;
-	var ty1 = (aabb.y - ray.y) / ray.dy;
-	var ty2 = (aabb.y + aabb.h - ray.y) / ray.dy;
-	
-	// Find the hit
-	return getIntercept(tx1, tx2, ty1, ty2);
-};
+
 
 //
 // Calculate pixel column.
@@ -188,6 +81,8 @@ Draw.calculatePixelColumn = function(hit, maxPixelHeight, tanVFov) {
 	return pixelColumn;
 		
 };
+
+
 
 // Render the world to the screen.
 
@@ -306,40 +201,6 @@ Draw.render = function(camera, map) {
 		};
 		
 		processHit( hit, 1.0 );
-		
-		// Shoot the ray at sprites
-		/*var hit = Draw.testRayAABB( ray,
-			{
-				x: 0.5,
-				y: 2,
-				w: 1,
-				h: 1
-			}
-		) || Draw.testRayAABB( ray,
-			{
-				x: -2.5,
-				y: 1,
-				w: 2,
-				h: 1
-			}
-		);
-		if (hit != null) {
-			// Distance = dist between hit & cam * Math.cos(rayAngle)
-			var diffX = (hit.x - ray.x);
-			var diffY = (hit.y - ray.y);
-			var dist   = Math.sqrt(diffX * diffX + diffY * diffY) * Math.cos(ray.angleOffset);
-			
-			// : BlockHeight      = 2.0
-			// : NormalizedHeight = (BlockHeight / 2) / (tan * distance)
-			// : DrawHeight       = (PixelHeight / 2) * NormalizedHeight;
-			var height = (Pixels.height / 2) / (tanVFov * dist);
-			
-			// Draw the pixel line
-			var y1 = Pixels.height / 2 - height / 2;
-			var y2 = Pixels.height / 2 + height / 2;
-			for (var y = y1; y < y2; y++)
-				Pixels.set(x, Math.floor(y), [0, 0, 0]);
-		}*/
 	}
 	
 	// Update the pixels
--- raycaster.js
+++ raycaster.js
@@ -135,3 +135,116 @@ Raycaster.shootRay = function(ray, map) {
 	// We didn't hit a thing	
 	return null;
 };
+
+
+
+// Test ray-vs-box.
+//
+// Parameters:
+//	ray = {
+//		x: 0,
+//		y: 0,
+//		dx: 0,
+//		dy: 0
+//	};
+//	aabb = {
+//		x: 0,
+//		y: 0,
+//		w: 0,
+//		h: 0 
+//	};
+//
+// Returns:
+//	null OR
+//	{
+//		x:    0, // Intersection x
+//		y:    0  // Intersection y
+//	};
+//
+Draw.testRayAABB = function(ray, aabb) {
+	
+	//
+	// Given tx1, tx2, ty1, ty2, return the intercept or null.
+	//
+	function getIntercept(tx1, tx2, ty1, ty2) {
+		// Calculate the max t
+		// This will be used to "throw out" values
+		// ie. if t > tMax, it's considered invalid
+		var tMax  = Math.max(tx1, tx2, ty1, ty2);
+		
+		// Throw out negative values (those hits are behind us)
+		if (tx1 < 0) tx1 = tMax + 1;
+		if (tx2 < 0) tx2 = tMax + 1;
+		if (ty1 < 0) ty1 = tMax + 1;
+		if (ty2 < 0) ty2 = tMax + 1;
+		
+		// Check tx1 bounds
+		if (ray.y + ray.dy * tx1 < aabb.y ||
+			  ray.y + ray.dy * tx1 > aabb.y + aabb.h)
+			tx1 = tMax + 1;
+	
+		// Check tx2 bounds
+		if (ray.y + ray.dy * tx2 < aabb.y ||
+			  ray.y + ray.dy * tx2 > aabb.y + aabb.h)
+			tx2 = tMax + 1;
+			
+		// Check ty1 bounds
+		if (ray.x + ray.dx * ty1 < aabb.x ||
+			  ray.x + ray.dx * ty1 > aabb.x + aabb.w)
+			ty1 = tMax + 1;
+	
+		// Check ty2 bounds
+		if (ray.x + ray.dx * ty2 < aabb.x ||
+			  ray.x + ray.dx * ty2 > aabb.x + aabb.w)
+			ty2 = tMax + 1;
+			
+		// Return the closest hit (if exists)
+		var tHit = Math.min(tx1, tx2, ty1, ty2);
+		if (tHit > tMax)
+			return null;
+		return {
+			x: ray.x + ray.dx * tHit,
+			y: ray.y + ray.dy * tHit
+		};
+	}
+	
+	// Handle vertical line
+	if (ray.dx === 0 && Math.abs(ray.dy) === 1) {
+	
+		// Check X bounds
+		if (ray.x < aabb.x || ray.x > aabb.x + aabb.w)
+			return null;
+			
+		// Calculate y intercepts
+		var ty1 = (aabb.y - ray.y) / ray.dy;
+		var ty2 = (aabb.y + aabb.h - ray.y) / ray.dy;
+		
+		// Find the hit
+		return getIntercept(-1, -1, ty1, ty2);
+	}
+	
+	// Handle horizontal line
+	if (Math.abs(ray.dx) === 1 && ray.dy === 0) {
+	
+		// Check Y bounds
+		if (ray.y < aabb.y || ray.y > aabb.y + aabb.h)
+			return null;
+			
+		// Calculate x intercepts
+		var tx1 = (aabb.x - ray.x) / ray.dx;
+		var tx2 = (aabb.x + aabb.w - ray.x) / ray.dx;
+		
+		// Find the hit
+		return getIntercept(tx1, tx2, -1, -1);
+	}
+	
+	// Handle every other line
+	// Calculate intercepts
+	var tx1 = (aabb.x - ray.x) / ray.dx;
+	var tx2 = (aabb.x + aabb.w - ray.x) / ray.dx;
+	var ty1 = (aabb.y - ray.y) / ray.dy;
+	var ty2 = (aabb.y + aabb.h - ray.y) / ray.dy;
+	
+	// Find the hit
+	return getIntercept(tx1, tx2, ty1, ty2);
+};
